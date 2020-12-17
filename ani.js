727916b022e57510e15eb216d74094f32b684499
songhun115 <hoho77113@daum.net>
2020-12-17 10:10:18 +0900

    마무리

--- ani.js
+++ ani.js
@@ -1,51 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
 
 setTimeout(function () {
-      // gsap.from(".animation_img1", {
-      //   scrollTrigger: {
-      //     trigger: "ani1",
-      //     toggleActions: 'play play play play',
-      //     markers:true
-      //   },
-      //   x: 800,
-      //   delay:1,
-      //   duration: 0.5
-      // });
-
       gsap.from(".animation_img1", {
         scrollTrigger: {
-          trigger: ".ani1",
-          toggleActions:'play play play play'
+          trigger: "ani1",
+          toggleActions: 'play play play play',
+          markers:true
         },
-        stagger: {
-          each: 0.1
-        },
+        x: 800,
         delay:1,
-        x: 800
+        duration: 0.5
       });
 
-        gsap.to(".animation_img2", {
-      scrollTrigger: {
-        trigger: ".section3 > div >.section_box > .content_box > .content_text > .content_main_text1",
-        toggleActions: 'play play play play',
-        markers:true
-      },
-      y: 70,
-      x:70,
-      delay:1,
-      duration: 0.5
-    });
-
-    gsap.from(".animation_img4", {
-      scrollTrigger: {
-        trigger: ".gsap44",
-        toggleActions: 'play play play play',
-        markers:true
-      },
-      y: 400,
-      delay:1,
-      duration: 0.5
-    });
     
    
 }, 500);
